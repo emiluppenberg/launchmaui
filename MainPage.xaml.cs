76ad3463fd39c6267165e1482e2d343f6f5b314f
twofourZ <uppenberg95@gmail.com>
2025-12-11 20:45:43 +0100

    removed /bin aswell

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using launchclient.Api;
+
+namespace launchmaui;
+
+public partial class MainPage : ContentPage
+{
+	private readonly ILaunchesApi launchesApi;
+
+	public MainPage(ILaunchesApi launchesApi)
+	{
+		InitializeComponent();
+		this.launchesApi = launchesApi;
+	}
+
+	public async void GetLaunches(object sender, EventArgs e)
+	{
+		var launches = await launchesApi.LaunchesListAsync();
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using launchclient.Api;
+using launchclient.Client;
+using Microsoft.Extensions.Logging;
+
+namespace launchmaui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+
+		builder.Services.AddSingleton<JsonSerializerOptions>(new JsonSerializerOptions
+		{
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			WriteIndented = false
+		});
+		builder.Services.AddSingleton<JsonSerializerOptionsProvider>();
+		builder.Services.AddSingleton<LaunchesApiEvents>();
+		builder.Services.AddHttpClient<ILaunchesApi, LaunchesApi>(c => { c.BaseAddress = new Uri(ClientUtils.BASE_ADDRESS); });
+
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
